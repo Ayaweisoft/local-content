8b06eebccd34c4616377754de4329fcf4448ff07
Codes Oracle <94334497+Oracle247@users.noreply.github.com>
2022-09-28 10:46:55 +0100

    changes

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,18 @@ import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-  <div class="scroll-smooth ">
+  <div class="scroll-smooth">
     <header class="fixed flex items-center justify-between w-full px-24 py-2 bg-white shadow-md">
       <div>Logo</div>
       <div class="flex gap-4">
-          <a href="#verifyMembership">Verify Membership</a>
-          <RouterLink to="/">Home</RouterLink>
-          <div class="px-2 py-2 text-xs bg-green-600 ">
-            <RouterLink to="/register">Become a Member</RouterLink>
-          </div>
+        <a href="#verifyMembership">Verify Membership</a>
+        <RouterLink to="/">Home</RouterLink>
+        <div class="px-2 py-2 text-xs bg-green-600">
+          <RouterLink to="/register">Become a Member</RouterLink>
+        </div>
       </div>
     </header>
-  
+
     <RouterView />
   </div>
 </template>
