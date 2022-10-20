4d6cc66c83ce7314caf5b42e7c2210883ec58cf2
Nicholasjr <nicholasduadei14@gmail.com>
2022-10-20 11:06:27 +0100

    registration done

--- src/views/Individual.vue
+++ src/views/Individual.vue
@@ -1,5 +1,6 @@
 <script setup>
     import { ref } from 'vue'
+    import router from '../router';
     import Individual1 from "../components/Individual-1.vue"
     import Individual2 from "../components/Individual-2.vue"
     import Individual3 from "../components/Individual-3.vue"
@@ -8,11 +9,19 @@
     import { useFetch } from '../composables/useFetch.js'
 
     const { data, error, loading, doFetch } = useFetch()
+
     const handleSubmit = async () => {
-        console.log("starting");
-        console.log(registration)
         await doFetch("https://local-content-server.herokuapp.com/api/v1/submit", registration);
-        console.log("fetched", { "data": data.value, "error": error.value, "loading": loading.value })
+        if (error.value) {
+            console.log("error")
+            if (error.value.errors?.message) {
+                console.log('returned errors', error.value.errors?.message)
+            }
+        }
+        else if (error.value == null && data.value.error == false) {
+            alert("your registration was successful")
+            router.push('/')
+        }
     }
 
     const step = ref(1)
@@ -27,8 +36,8 @@
       email: '',
 
       meta: {
-        gender: 'M',
-        maritalStatus: 'single',
+        gender: '',
+        maritalStatus: '',
         religion:'',
         dob: '',
         nationality:'',
@@ -53,13 +62,7 @@
           name: '',
           date: '',
           signature: ''
-      },
-
-      authority: '',
-      state: '',
-      status: 'pending',
-      kiv: '',
-      signature: ''
+      }
     }
 </script>
   
@@ -67,11 +70,15 @@
 <template>
     <div class="px-4 sm:px-16 text-sm">
         <Progress :step="step" />
-        <form @submit.prevent="handleSubmit" action="" class="">
+        <div class="error text-center text-red-600 mb-8">
+            {{ error && "Something went wrong" }}
+            <!-- {{ error && error.errors?.message ? error.errors?.message : error ? error : "" }} -->
+        </div>
+        <div action="" class="">
             <Individual1 :registration="registration" @nextStep="step++" v-if="step == 1" />
             <Individual2 :registration="registration" @prevStep="step--" @nextStep="step++" v-else-if="step == 2" />
-            <Individual3 :registration="registration" @prevStep="step--" v-else-if="step == 3" />
-        </form>
+            <Individual3 :registration="registration" @prevStep="step--" @submit_form="handleSubmit" v-else-if="step == 3" />
+        </div>
     </div>
 </template>
   
